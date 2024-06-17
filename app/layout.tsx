7ac1bf8b4a580cb2bd1e5f612089da6263e88b2a
davidherrera67 <herreracostadavid@gmail.com>
2024-06-17 11:45:13 +0200

    Setup Project: Add Home & Helper components. Also use of Poppins Google Font.

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppinsfont = Poppins({ 
+  weight: ['200','300','400','500','600','700','800'],
+  subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Portfolio | David Herrera Costa",
+  description: "Portfolio Project Website created with Next.js, React, Tailwind CSS and Typescript",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppinsfont.className} bg-[#000000d8]`}>{children}</body>
+    </html>
+  );
+}
